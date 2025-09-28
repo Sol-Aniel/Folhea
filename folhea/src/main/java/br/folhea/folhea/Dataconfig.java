8ab9a50c8572c0b67645fb409b6a9fb81e92b9f1
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2025-09-27 22:05:57 -0300

    add arquivos iniciais com spring boot

---
+++ folhea/src/main/java/br/folhea/folhea/Dataconfig.java
@@ -0,0 +1,11 @@
+package br.folhea.folhea;
+
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class Dataconfig {
+
+
+
+}
---
+++ folhea/src/main/java/br/folhea/folhea/FolheaApplication.java
@@ -0,0 +1,13 @@
+package br.folhea.folhea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolheaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolheaApplication.class, args);
+	}
+
+}
