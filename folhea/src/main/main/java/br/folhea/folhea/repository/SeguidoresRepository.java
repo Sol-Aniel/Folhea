91601f5b2f65d16e284ac2a6b6fa0d4cd26ad67f
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2025-11-26 20:35:36 -0300

    Delete folhea/src/main/main directory
    Signed-off-by: laura1050santos <64499700+laura1050santos@users.noreply.github.com>

--- folhea/src/main/main/java/br/folhea/folhea/repository/SeguidoresRepository.java
+++
@@ -1,8 +0,0 @@
-package br.folhea.folhea.repository;
-
-import br.folhea.folhea.model.Seguidores;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeguidoresRepository extends JpaRepository<Seguidores,Long> {
-
-}
